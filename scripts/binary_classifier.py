56c3d60448f5a04adbeab69146999b55da367bd6
Klitos <kleitos.savva.97@gmail.com>
2022-06-22 08:48:59 +0100

    Added option to train on one mass point and test on others

--- scripts/binary_classifier.py
+++ scripts/binary_classifier.py
@@ -1,5 +1,5 @@
 from UserCode.sig_vs_bkg_discriminator.Dataframe import Dataframe
-from UserCode.sig_vs_bkg_discriminator.plotting import DrawROCCurve, DrawBDTScoreDistributions, DrawFeatureImportance
+from UserCode.sig_vs_bkg_discriminator.plotting import DrawROCCurve, DrawBDTScoreDistributions, DrawFeatureImportance, DrawVarDistribution
 import argparse
 import pickle as pkl
 import pandas as pd
@@ -18,13 +18,17 @@ args = parser.parse_args()
 # Set up variables to use for training
 #variables = ["mt_tot","pt_1","pt_2","met","m_vis","n_deepbjets","pt_tt","mt_lep","met_dphi_1","met_dphi_2","jet_pt_1/pt_1","jet_pt_2/pt_2"]
 
-variables = ["pt_1", "pt_2","pt_3","pt_4","E_1","E_2","E_3","E_4","eta_1","eta_2","eta_3","eta_4",
-"phi_1","phi_2","phi_3","phi_4","dphi_12","dphi_13","dphi_14","dphi_23","dphi_24","dphi_34","dR_12","dR_13","dR_14","dR_23",
+variables = ["pt_1", "pt_2","pt_3","pt_4","dphi_12","dphi_13","dphi_14","dphi_23","dphi_24","dphi_34","dR_12","dR_13","dR_14","dR_23",
 "dR_24","dR_34","mt_1","mt_2","mt_3","mt_4","mt_lep_12","mt_lep_13","mt_lep_14","mt_lep_23","mt_lep_24","mt_lep_34",
-"mvis_12","mvis_13","mvis_14","mvis_23","mvis_24","mvis_34","mvis_min_dphi_1", "mvis_min_dphi_2","mvis_min_dR_1", "mvis_min_dR_2", 
-"mvis_min_sum_dphi_1","mvis_min_sum_dphi_2","mvis_min_sum_dR_1","mvis_min_sum_dR_2","pt_min_dphi_1","pt_min_dphi_2","pt_min_dR_1", 
-"pt_min_dR_2","pt_min_sum_dphi_1","pt_min_sum_dphi_2","pt_min_sum_dR_1","pt_min_sum_dR_2","p_min_dphi_1","p_min_dphi_2","p_min_sum_dphi_1",
-"p_min_sum_dphi_2","p_min_dR_1","p_min_dR_2","p_min_sum_dR_1","p_min_sum_dR_2","q_1","q_2","q_3","q_4"]
+"mvis_12","mvis_13","mvis_14","mvis_23","mvis_24","mvis_34","q_1","q_2","q_3","q_4"]
+
+# variables = ["pt_1", "pt_2","pt_3","pt_4","eta_1","eta_2","eta_3","eta_4",
+# "phi_1","phi_2","phi_3","phi_4","dphi_12","dphi_13","dphi_14","dphi_23","dphi_24","dphi_34","dR_12","dR_13","dR_14","dR_23",
+# "dR_24","dR_34","mt_1","mt_2","mt_3","mt_4","mt_lep_12","mt_lep_13","mt_lep_14","mt_lep_23","mt_lep_24","mt_lep_34",
+# "mvis_12","mvis_13","mvis_14","mvis_23","mvis_24","mvis_34","mvis_min_dphi_1", "mvis_min_dphi_2","mvis_min_dR_1", "mvis_min_dR_2", 
+# "mvis_min_sum_dphi_1","mvis_min_sum_dphi_2","mvis_min_sum_dR_1","mvis_min_sum_dR_2","pt_min_dphi_1","pt_min_dphi_2","pt_min_dR_1", 
+# "pt_min_dR_2","pt_min_sum_dphi_1","pt_min_sum_dphi_2","pt_min_sum_dR_1","pt_min_sum_dR_2","p_min_dphi_1","p_min_dphi_2","p_min_sum_dphi_1",
+# "p_min_sum_dphi_2","p_min_dR_1","p_min_dR_2","p_min_sum_dR_1","p_min_sum_dR_2","q_1","q_2","q_3","q_4"]
 
 abs_variables = ["dphi_12","dphi_13","dphi_14","dphi_23","dphi_24","dphi_34","dR_12","dR_13","dR_14","dR_23",
 "dR_24","dR_34"]
@@ -44,6 +48,18 @@ if not (args.load and os.path.isfile("dataframes/{}_{}.pkl".format(args.channel,
   print "Length =",len(sig_df.dataframe)
   print "Weight Normalisation =",sig_df.dataframe.loc[:,"weights"].sum()
   
+  # Add Test dataframe
+  add_sig_df = Dataframe()
+  add_sig_df.LoadRootFilesFromJson("json_selection/{}_{}_sig_test.json".format(args.channel,args.year),variables)
+  add_sig_df.NormaliseWeights()
+  add_sig_df.dataframe.loc[:,"y"] = 1
+  for i in abs_variables: 
+    add_sig_df.dataframe[i] = add_sig_df.dataframe[i].abs()
+  print "Additional Samples Dataframe"
+  print add_sig_df.dataframe.head()
+  print "Length =",len(add_sig_df.dataframe)
+  print "Weight Normalisation =",add_sig_df.dataframe.loc[:,"weights"].sum()
+  
   # Add background data
   bkg_df = Dataframe()
   bkg_df.LoadRootFilesFromJson("json_selection/{}_{}_bkg.json".format(args.channel,args.year),variables)
@@ -57,27 +73,12 @@ if not (args.load and os.path.isfile("dataframes/{}_{}.pkl".format(args.channel,
   # Combine dataframes
   df_total = pd.concat([bkg_df.dataframe,sig_df.dataframe],ignore_index=True, sort=False)
   
-  df_total['pt_1_mt_lep_12'] = df_total['pt_1']/df_total['mt_lep_12']
-  df_total['pt_1_mt_lep_13'] = df_total['pt_1']/df_total['mt_lep_13']
-  df_total['pt_1_mt_lep_14'] = df_total['pt_1']/df_total['mt_lep_14']
-  
-  df_total['pt_2_mt_lep_12'] = df_total['pt_2']/df_total['mt_lep_12']
-  df_total['pt_2_mt_lep_23'] = df_total['pt_2']/df_total['mt_lep_23']
-  df_total['pt_2_mt_lep_24'] = df_total['pt_2']/df_total['mt_lep_24']
-  
-  df_total['pt_3_mt_lep_13'] = df_total['pt_3']/df_total['mt_lep_13']
-  df_total['pt_3_mt_lep_23'] = df_total['pt_3']/df_total['mt_lep_23']
-  df_total['pt_3_mt_lep_34'] = df_total['pt_3']/df_total['mt_lep_34']
-  
-  df_total['pt_4_mt_lep_14'] = df_total['pt_4']/df_total['mt_lep_14']
-  df_total['pt_4_mt_lep_24'] = df_total['pt_4']/df_total['mt_lep_24']
-  df_total['pt_4_mt_lep_34'] = df_total['pt_4']/df_total['mt_lep_34']
   
   for i in abs_variables: 
     df_total[i] = df_total[i].abs()
     
-  df_total.drop(["pt_1", "pt_2","pt_3","pt_4","mt_lep_12","mt_lep_13","mt_lep_14","mt_lep_23","mt_lep_24","mt_lep_34"],
-  inplace = True,axis=1)
+  # df_total.drop(["pt_1", "pt_2","pt_3","pt_4","mt_lep_12","mt_lep_13","mt_lep_14","mt_lep_23","mt_lep_24","mt_lep_34"],
+  # inplace = True,axis=1)
   
   print "Total Dataframe"
   print df_total.head()
@@ -88,6 +89,7 @@ else:
   print "<< Loading in dataframe >>"
   df_total = pd.read_pickle("dataframes/binary_{}_{}.pkl".format(args.channel,args.year))
 
+DrawVarDistribution(df_total,2,'q_4',-2,2,"bc_q_4_distribution")
 # Set up train and test separated dataframes
 train, test = train_test_split(df_total,test_size=0.5, random_state=42)
 
@@ -95,9 +97,18 @@ y_train = train.loc[:,"y"]
 wt_train = train.loc[:,"weights"]
 X_train = train.drop(["y","weights"],axis=1)
 
+
 y_test = test.loc[:,"y"]
 wt_test = test.loc[:,"weights"]
 X_test = test.drop(["y","weights"],axis=1)
+
+
+# comment this properly
+add_test = test[(test.loc[:,"y"]==0)]
+add_df_total = pd.concat([add_test,add_sig_df.dataframe],ignore_index=True, sort=False)
+add_y_test = add_df_total.loc[:,"y"]
+add_wt_test = add_df_total.loc[:,"weights"]
+add_X_test = add_df_total.drop(["y","weights"],axis=1)
 
 
 # def algorithm_pipeline(X_train_data, X_test_data, y_train_data, y_test_data, 
@@ -165,7 +176,12 @@ print "<< Training finished >>"
 # ROC curve
 probs = xgb_model.predict_proba(X_test)
 preds = probs[:,1]
-DrawROCCurve(y_test,preds,wt_test)
+DrawROCCurve(y_test,preds,wt_test,"roc_curve_original")
+
+
+probs_ = xgb_model.predict_proba(add_X_test)
+preds_ = probs_[:,1]
+DrawROCCurve(add_y_test,preds_,add_wt_test,"roc_curve_Additional_Signal_Samples")
 
 # BDT scores
 t0 = test.loc[(test.loc[:,"y"]==0)]
@@ -182,4 +198,6 @@ preds1 = probs1[:,1]
 DrawBDTScoreDistributions({"background":{"preds":preds0,"weights":wt0},"signal":{"preds":preds1,"weights":wt1}})
 
 # Feature importance
-DrawFeatureImportance(xgb_model)
+
+DrawFeatureImportance(xgb_model,"gain","bc_feature_importance_gain")
+DrawFeatureImportance(xgb_model,"weight","bc_feature_importance_weight")
